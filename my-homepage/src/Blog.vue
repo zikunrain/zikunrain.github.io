02ccf7a278a512ce0df0a2eae9a8d5d8fee3fb66
Zikun Deng <769670984@qq.com>
2018-05-30 15:45:05 +0800

    blog

--- my-homepage/src/Blog.vue
+++ my-homepage/src/Blog.vue
@@ -1,16 +1,176 @@
 <template>
   <div id="blog">
-    {{msg}}
+    <div class="blog-header">
+      <span class="blog-header-font">
+        Zikun Deng's Mini Blog
+      </span>
+    </div>
+    <div class="blog-content">
+      <div class="doc-title-list">
+        <div class="doc-title-list-header">
+          <span class="header-text">Document List</span>
+        </div>
+        <div class="doc-title-list-cont">
+          <div class="doc-title" v-for="(t, index) in docTitles" :key="index"
+            :title="t"
+            @mouseenter="hoverTitle(index)"
+            @mouseleave="hoverTitle(-1)"
+            @click="selectDoc(index)"
+            :class="{ hover: hoveringTitleIndex === index, selected: selectedDoc.title === t }">
+            {{t}}
+          </div>
+        </div>
+      </div>
+      <div class="doc">
+        <Doc :doc="selectedDoc"></Doc>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Doc from './components/Blog/Doc'
+import blogs from './assets/blogs.json'
+
 export default {
   name: 'Blog',
   data () {
     return {
-      msg: 'I am blog to be updated'
+      msg: 'I am blog to be updated',
+      blogs: blogs.Blogs,
+      hoveringTitleIndex: -1,
+      selectedDoc: blogs.Blogs[0]
     }
+  },
+  computed: {
+    docTitles () {
+      return this.blogs.map(b => b.title)
+    }
+  },
+  methods: {
+    hoverTitle (titleIndex) {
+      this.hoveringTitleIndex = titleIndex
+    },
+    selectDoc (index) {
+      this.selectedDoc = this.blogs[index]
+    }
+  },
+  components: {
+    Doc
   }
 }
 </script>
+
+<style lang="scss">
+body, #app {
+  background-color: #fff;
+  font-family: Lato;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+#blog {
+  position: relative;
+  width: 100vw;
+
+  .blog-header {
+    position: relative;
+    width: calc(100%);
+    height: 50px;
+    background-color: #3385ff;
+    box-shadow: 0 5px 5px 0 rgba(0,0,0,.11);
+    .blog-header-font {
+      position: relative;
+      height: 50px;
+      padding-left: 20px;
+      font-size: 20px;
+      font-family: Lato;
+      color: #fff;
+      line-height: 50px;
+      font-weight: 700;
+    }
+  }
+
+  .blog-content {
+    max-width: 700px;
+    margin: auto;
+
+    .doc-title-list {
+      position: relative;
+      width: calc(100%);
+      height: 200px;
+      margin: 20px 0;
+      background-color: #fff;
+      box-shadow: 0 6px 8px 0 rgba(0,0,0,0.2);
+      border: 1px solid #ddd;
+      border-radius: 4px;
+
+      .doc-title-list-header {
+        position: relative;
+        width: calc(100%);
+        height: 39px;
+        border-bottom: 1px solid #ddd;
+        background-color: #f7f7f7;
+        .header-text {
+          position: relative;
+          height: 100%;
+          color: #666;
+          font-family: Lato;
+          line-height: 39px;
+          padding-left: 10px;
+        }
+      }
+
+      .doc-title-list-cont {
+        position: relative;
+        width: calc(100% - 20px);
+        height: calc(100% - 20px - 40px);
+        margin: 10px;
+        overflow-x: hidden;
+        overflow-y: auto;
+
+        .doc-title {
+          position: relative;
+          margin: 2px 0px;
+          width: calc(100% - 10px);
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          height: 22px;
+          line-height: 22px;
+          font-family: Lato;
+          color: #666;
+          padding-left: 5px;
+          border: 1px solid #fff;
+          border-radius: 2px;
+          cursor: pointer;
+          transition: all 200ms;
+
+          &.hover {
+            background-color: #e4efff;
+          }
+
+          &.selected {
+            border: 1px solid #3385ff;
+          }
+        }
+      }
+    }
+
+    .doc {
+      position: relative;
+      width: calc(100%);
+      margin: 20px 0;
+      background-color: #fff;
+      box-shadow: 0 6px 8px 0 rgba(0,0,0,0.2);
+      border: 1px solid #ddd;
+    }
+  }
+}
+</style>
---
+++ my-homepage/src/components/Blog/Doc.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="doc-view">
+    <div class="doc-header">
+      <span class="header-text">Selected Document</span>
+    </div>
+    <div class="doc-content">
+      <div class="title">
+        {{doc.title}}
+      </div>
+      <div class="pic">
+        <img :src='src'>
+      </div>
+      <div class="text">
+        <div v-html="doc.content"></div>
+      </div>
+      <div class="ref">
+        [1] {{doc.ref}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['doc'],
+  computed: {
+    src () {
+      return require(`../../assets/${this.doc.pic}`)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.doc-view {
+  position: relative;
+  width: calc(100%);
+
+  .doc-header {
+    position: relative;
+    width: calc(100%);
+    height: 39px;
+    border-bottom: 1px solid #ddd;
+    background-color: #f7f7f7;
+    .header-text {
+      padding-left: 10px;
+      position: relative;
+      height: calc(100%);
+      line-height: 39px;
+      color: #666;
+    }
+  }
+  .doc-content {
+    position: relative;
+    padding: 15px;
+    width: calc(100% - 30px);
+    .title {
+      height: 36px;
+      // line-height: 36px;
+      font-size: 18px;
+      font-weight: 700;
+      color: #333;
+      font-family: Lato;
+      text-align: center;
+      margin: 10px 0 20px 0;
+      word-wrap: break-word;
+    }
+    .pic {
+      position: relative;
+      width: 100%;
+      img {
+        position: relative;
+        width: 100%;;
+      }
+    }
+    .text {
+      p {
+        word-break: keep-all;
+      }
+    }
+    .ref {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+}
+
+</style>
