9455e05414a48f12efc7114f3f5ff9e3fb839443
Zikun Deng <769670984@qq.com>
2018-06-01 23:19:02 +0800

    61

--- my-homepage/src/Blog.vue
+++ my-homepage/src/Blog.vue
@@ -173,4 +173,20 @@ body, #app {
     }
   }
 }
+
+.doc-title-list-cont::-webkit-scrollbar {/*滚动条整体样式*/
+    width: 4px;     /*高宽分别对应横竖滚动条的尺寸*/
+    height: 4px;
+}
+.doc-title-list-cont::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+    border-radius: 5px;
+    -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+    background: rgba(0,0,0,0.2);
+}
+.doc-title-list-cont::-webkit-scrollbar-track {/*滚动条里面轨道*/
+    -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+    border-radius: 0;
+    background: rgba(0,0,0,0.1);
+}
+
 </style>
